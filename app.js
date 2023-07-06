c8b1c592a163b1576b1ec6f253c7dcabe3e6f606
Markus Uusitalo <markus.uusitalo@aalto.fi>
2023-07-06 21:02:19 +0300

    Changed response

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("Hello you!");
+  return new Response("Moi Laura, Haluatko iso seksi?");
 };
 
 serve(handleRequest, { port: 7777 });
